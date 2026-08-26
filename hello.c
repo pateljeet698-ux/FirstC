c7f570e7a8cbc30fec4f32b77e115b489558d7ae
Jeet <pateljeet698@gmail.com>
2026-08-26 14:18:02 +0530

    Add clockwise rotating shuriken animation

--- hello.c
+++ hello.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void)
 {
-    printf("Hello, World!\n");
+    char shuriken[] = {'|', '/', '-', '\\', '|', '/', '-', '\\'};
+    
+    printf("Clockwise rotating Shuriken:\n");
+    
+    for (int i = 0; i < 40; i++)
+    {
+        printf("\r%c", shuriken[i % 8]);
+        fflush(stdout);
+        usleep(100000);  // 100ms delay
+    }
+    
+    printf("\nDone!\n");
     return 0;
 }
